e1661a1c8fd1f4048fe8136dc0fc1e2d30ffec8c
alson <filsalouna@gmail.com>
2024-12-20 03:33:30 +0300

    Added tree_2_3_4

---
+++ src/main/java/org/example/dsa/tree_2_3_4/DataItem.java
@@ -0,0 +1,14 @@
+package org.example.dsa.tree_2_3_4;
+
+public class DataItem {
+    public long dData;
+
+    public DataItem(long dd) {
+        dData = dd;
+    }
+
+    public void displayItem() {
+        System.out.println("/"+dData);
+    }
+
+}
