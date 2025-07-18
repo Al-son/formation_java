bdaa00721ccf2296faec3e1fe574b19b38969b58
Fils Alouna <filsalouna@gmail.com>
2025-07-18 17:49:54 +0300

    Added heap

---
+++ src/main/java/org/example/dsa/heap/Node.java
@@ -0,0 +1,17 @@
+package org.example.dsa.heap;
+
+public class Node {
+    private int iDate;
+
+    public Node(int key){
+        iDate = key;
+    }
+
+    public int getKey() {
+        return iDate;
+    }
+
+    public void setKey(int id) {
+        iDate = id;
+    }
+}
